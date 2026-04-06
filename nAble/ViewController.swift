b293c90e8dd91ff6bbf93adaac5085e32568945e
Elene <elenedgebuadze83@gmail.com>
2026-04-06 14:13:35 +0400

    Add .gitignore and remove Xcode user-specific files

---
+++ nAble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nAble
+//
+//  Created by Eorime on 05.04.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
