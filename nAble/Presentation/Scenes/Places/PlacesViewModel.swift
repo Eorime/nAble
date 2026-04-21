557676371f4c19926212185749e6dd24480f0e24
Eorime <elenedgebuadze83@gmail.com>
2026-04-21 17:31:30 +0400

    Write save place, remove place and load saved places functions

--- nAble/Presentation/Scenes/Places/PlacesViewModel.swift
+++ nAble/Presentation/Scenes/Places/PlacesViewModel.swift
@@ -6,16 +6,18 @@ class PlacesViewModel {
     private let fetchPlacesUseCase: FetchNearbyPlacesUseCase
     private let savePlaceUseCase: SavePlaceUseCase
     private let removeSavedPlaceUseCase: RemoveSavedPlaceUseCase
+    private let fetchSavedPlacesUseCase: FetchSavedPlacesUseCase
 
     var onPlacesLoaded: (([Place]) -> Void)?
     var onSavedPlacesLoaded: (([Place]) -> Void)?
     var onError: ((Error) -> Void)?
     
-    init(getCurrentLocation: GetCurrentLocationUseCaseProtocol, fetchPlacesUseCase: FetchNearbyPlacesUseCase, savePlaceUseCase: SavePlaceUseCase, removeSavedPlaceUseCase: RemoveSavedPlaceUseCase) {
+    init(getCurrentLocation: GetCurrentLocationUseCaseProtocol, fetchPlacesUseCase: FetchNearbyPlacesUseCase, savePlaceUseCase: SavePlaceUseCase, removeSavedPlaceUseCase: RemoveSavedPlaceUseCase, fetchSavedPlacesUseCase: FetchSavedPlacesUseCase) {
         self.getCurrentLocation = getCurrentLocation
         self.fetchPlacesUseCase = fetchPlacesUseCase
         self.savePlaceUseCase = savePlaceUseCase
         self.removeSavedPlaceUseCase = removeSavedPlaceUseCase
+        self.fetchSavedPlacesUseCase = fetchSavedPlacesUseCase
     }
     
     func loadNearbyPlaces() {
@@ -41,4 +43,44 @@ class PlacesViewModel {
             }
         }
     }
+    
+    func savePlace(userId: String, place: Place) {
+        Task {
+            do {
+                try await savePlaceUseCase.execute(userId: userId, place: place)
+            } catch {
+                DispatchQueue.main.async {
+                    self.onError?(error)
+                }
+            }
+        }
+    }
+    
+    func removeSavedPlace(userId: String, placeId: String) {
+        Task {
+            do {
+                try await removeSavedPlaceUseCase.execute(userId: userId, placeId: placeId)
+            } catch {
+                DispatchQueue.main.async {
+                    self.onError?(error)
+                }
+            }
+        }
+    }
+    
+    func loadSavedPlaces(userId: String) {
+        Task {
+            do {
+                let places = try await fetchSavedPlacesUseCase.execute(userId: userId)
+                DispatchQueue.main.async {
+                    self.onSavedPlacesLoaded?(places)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    self.onError?(error)
+                }
+            }
+        }
+    }
+    
 }
